a94f5a2328ee8d3f145f81dee06ab2558fa89f73
Mitch Spradlin <mitch@spradlin.net>
2016-06-23 09:54:37 -0500

    Fixing setup script to use python3

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Sets up the virtualenv right
-virtualenv --no-site-packages venv
+virtualenv --no-site-packages -p python3 venv
 
 # Sets up packages appropriately for the bot
 source venv/bin/activate
 pip install python-telegram-bot
 pip install pillow
+pip install requests
 
 # Installs the fonts needed
 apt-get install libfreetype6-dev
